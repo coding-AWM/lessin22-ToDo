e8dc94c5bd7ac078cf835a7c6aa7ff41fa6c3989
Alex Moiseev <coding-awm@mail.ru>
2020-07-10 21:54:21 +0600

    запутался в 65 строке. Элем не впихиватеся. прошу подсказку

--- script.js
+++ script.js
@@ -1,92 +1,114 @@
 'use strict';
 
 class Todo {
-	constructor(form, input, todoList, todoCompleted) {
-		this.form = document.querySelector(form);
-		this.input = document.querySelector(input);
-		this.todoList = document.querySelector(todoList);
-		this.todoCompleted = document.querySelector(todoCompleted);
+  constructor(form, input, todoList, todoCompleted) {
+    this.form = document.querySelector(form);
+    this.input = document.querySelector(input);
+    this.todoList = document.querySelector(todoList);
+    this.todoCompleted = document.querySelector(todoCompleted);
     this.todoData = new Map(JSON.parse(localStorage.getItem('todoList')));
     this.todoСontainer = document.querySelector('.todo-container');
-    
-  }
-  
-  
-  addToStorage () {
-    localStorage.setItem('todoList', JSON.stringify([...this.todoData]))
-   
+    this.currentKey;
   }
 
-	render() {
+
+
+  addToStorage() {
+    localStorage.setItem('todoList', JSON.stringify([...this.todoData]))
+
+  }
+
+  render() {
     this.todoList.textContent = '';
     this.todoCompleted.textContent = '';
-    this.todoData.forEach(this.createItem);//или сюда this.createItem,this
+    this.todoData.forEach(this.createItem); //или сюда this.createItem,this
     this.addToStorage();
-	}
+  }
 
-	createItem = (todo) => { //или тут сделать стрелочную
-		const li = document.createElement('li');
+  createItem = (todo) => { //или тут сделать стрелочную
+    const li = document.createElement('li');
     li.classList.add('todo-item');
     li.key = todo.key;
-		li.insertAdjacentHTML('beforeend', `
+    li.insertAdjacentHTML('beforeend', `
     <span class="text-todo">${todo.value}</span>
 				<div class="todo-buttons">
 					<button class="todo-remove"></button>
 					<button class="todo-complete"></button>
         </div>`);
-		if (todo.completed) {
-			this.todoCompleted.append(li);
-		} else {
-			this.todoList.append(li);
-		}
-	}
-
-	addTodo(e) {
-		e.preventDefault();
-
-		if (this.input.value.trim()) {
-			const newTodo = {
-				value: this.input.value,
-				completed: false,
-				key: this.generateKey(),
-			};
-			this.todoData.set(newTodo.key, newTodo);
-			this.render();
-
-		}
-
-	}
-
-	generateKey() {
-		return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    if (todo.completed) {
+      this.todoCompleted.append(li);
+    } else {
+      this.todoList.append(li);
+    }
   }
-  
-  completedItem () {
+
+  addTodo(e) {
+    e.preventDefault();
+
+    if (this.input.value.trim()) {
+      const newTodo = {
+        value: this.input.value,
+        completed: false,
+        key: this.generateKey(),
+      };
+      this.todoData.set(newTodo.key, newTodo);
+      this.render();
+
+    }
+
+  }
+
+  generateKey() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+
+  completedItem(currentKey) {
     //завершает. через ворич перебрать все элементы this.todoData, найтти элемент которому соответствует ключ
     //и поменять значение completed: false, => true  и наоборот
+    this.todoData.forEach(elem => {
+      if (this.todoItem.key = currentKey) {
+        this.todoItem.completed = true
+      }
+    });
+    console.log('комплитю', currentKey);
+
+
 
   }
 
-  deleteItem () {
-    //удаяляет.найти по ключу, удалить из nevMap, сделать рендер
-
+  deleteItem(currentKey) {
+    //удаяляет.найти по ключу, удалить из newMap, сделать рендер
+    console.log('удаляю', currentKey);
   }
 
-  handler () {
+  handler() {
     this.todoItem = document.querySelectorAll('.todo-item');
-    console.log('this.todoItem: ', this.todoItem[2].key);
-    
+    // console.log('this.todoItem: ', this.todoItem[2].key);
+
     //метод будет определять на какую из кнопок тыкнули, делегирование
     this.todoСontainer.addEventListener('click', (event) => {
       let foo = event.target.closest('.todo-item');
-      console.log('foo: ', foo[0]);
+      this.currentKey = foo.key;
+      if (event.target.matches('.todo-remove')) {
+        this.deleteItem(this.currentKey);
+      }
+      if (event.target.matches('.todo-complete')) {
+        this.completedItem(this.currentKey);
+      }
     })
+
+
   }
 
-  
+  // slider.addEventListener('mouseout', (event) => { //mouseleave не подойдёт
+  //   if (event.target.matches('.portfolio-btn') ||
+  //     event.target.matches('.dot')) {
+  //     startSlides();
+  //   }
+  // });
 
 
-	init() {
+  init() {
     this.form.addEventListener('submit', this.addTodo.bind(this));
     this.render()
     this.todoData.forEach((value, key) => {
@@ -95,10 +117,9 @@ class Todo {
     this.handler();
     // console.log(this.todoList);
     // console.log(this.todoItem);
-    
-	}
+
+  }
 }
 
 const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed');
 todo.init();
-
